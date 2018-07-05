3b8642c3fce40bd9fa8d290d41daf02f6114bc01
ingleo <clgrhc@gmail.com>
2018-07-05 04:50:24 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public/my-dream-app'));
+
+app.get('/', function (req, res) {
+    res.redirect('/');
+})
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones sobre el puerto ${port}`);
+});
